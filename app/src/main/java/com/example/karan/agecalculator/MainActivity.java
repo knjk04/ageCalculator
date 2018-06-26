5d94f7b8cd7daf6a9e9bf8454ce30c5c7ffea1b6
knjk04 <karan.n.j.kumar@gmail.com>
2018-06-26 17:45:42 +0100

    Added confirm button

--- app/src/main/java/com/example/karan/agecalculator/MainActivity.java
+++ app/src/main/java/com/example/karan/agecalculator/MainActivity.java
@@ -1,24 +1,18 @@
 package com.example.karan.agecalculator;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CalendarView;
 import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.Toast;
-
 import java.util.Calendar;
-import java.util.TimeZone;
 
 public class MainActivity extends AppCompatActivity {
 
     private DatePicker datePicker;
-//    private CalendarView calendarView;
+    private Button button;
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
@@ -28,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         datePicker = findViewById(R.id.datePicker);
+        button = findViewById(R.id.confirmButton);
 
         //Set max date to be 3 years prior
         int minAge = 3; //min age is 3 years old
@@ -42,25 +37,17 @@ public class MainActivity extends AppCompatActivity {
                 String date = day + "/" + month + "/" + year;
                 Log.d(TAG, "onSelectedDayChange: date : " + date);
 
-                int selectedDates[] = {day, month, year};
-                startCalc(selectedDates);
+                final int selectedDates[] = {day, month, year};
+
+                button.setOnClickListener(new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startCalc(selectedDates);
+                    }
+                });
+
             }
         });
-
-
-
-//        calendarView = findViewById(R.id.calendarView);
-//        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-//            @Override
-//            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
-//                month++; //0 indexed
-//                String date = day + "/" + month + "/" + year;
-//                Log.d(TAG, "onSelectedDayChange: date : " + date);
-//
-//                int selectedDates[] = {day, month, year};
-//                startCalc(selectedDates);
-//            }
-//        });
     }
 
     private void startCalc(int[] selectedDates) {
